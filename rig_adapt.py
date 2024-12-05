2410a4ca3d19edc8e0faaf35ca7e2e0616289477
ByAnonymus <byanonymus1@gmail.com>
2024-12-05 16:17:23 +0200

    start on arms

--- rig_adapt.py
+++ rig_adapt.py
@@ -7,6 +7,7 @@ def copy_bone_props(new_bone_name, old_bone, **kwargs):
     set_as_parent = kwargs.get("set_as_parent", False)
     world_bone = kwargs.get("world_bone", False)
     bone_for_length = kwargs.get("bone_for_length", None)
+    align = kwargs.get("align", False)
     edit_bones = old_bone.id_data.edit_bones
     new_bone = edit_bones.new(name=new_bone_name)
     if world_bone == False:
@@ -28,28 +29,46 @@ def copy_bone_props(new_bone_name, old_bone, **kwargs):
         new_bone.tail[0] = new_bone.head[0]
         new_bone.tail[2] = new_bone.head[2]
         new_bone.length = -1 * old_bone.length
+        if parent != None:
+            new_bone.parent = edit_bones[parent]
+            if align:
+                new_bone.align_orientation(other=new_bone.parent)
+        if length != None:
+            if bone_for_length == None:
+                new_bone.length = old_bone.length * length
+            else:
+                new_bone.length = edit_bones[bone_for_length].length * length
 def add_stretch(bone, bone_to_stetch_to, ):
+    bpy.ops.object.mode_set(mode='OBJECT')
     bpy.ops.object.mode_set(mode='EDIT', toggle=False)
     pose_bones = bpy.context.active_object.pose.bones
-    rest_length = bone.length
-    bone_name = bone.name
+    rest_length = bpy.context.active_object.data.edit_bones[bone].length
     bpy.ops.object.mode_set(mode='POSE', toggle=False)
-    constraint = pose_bones[bone_name].constraints.new('STRETCH_TO')
+    constraint = pose_bones[bone].constraints.new('STRETCH_TO')
     constraint.target = bpy.context.active_object
     constraint.subtarget = bone_to_stetch_to
     constraint.rest_length = rest_length
-def add_copy_transforms(bone, bone_to_stetch_to, influence):
+def add_copy_transforms(bone, bone_to_stetch_to, influence, constraint_type, **kwargs):
+    bpy.ops.object.mode_set(mode='OBJECT')
+    bpy.ops.object.mode_set(mode='EDIT', toggle=False)
     pose_bones = bpy.context.active_object.pose.bones
+    world = kwargs.get("world", False)
+    head_tail = kwargs.get("head_tail", 0)
+    bone_name = bone
     # rest_length = bone.length
     bpy.ops.object.mode_set(mode='POSE', toggle=False)
-    pose_bones[bone.name].constraints.new('COPY_TRANSFORMS')
-    constraint = pose_bones[bone.name].constraints["Copy Transforms"]
+    constraint = pose_bones[bone_name].constraints.new(constraint_type)
     constraint.target = bpy.context.active_object
     constraint.subtarget = bone_to_stetch_to
-    constraint.target_space = 'LOCAL'
-    constraint.owner_space = 'LOCAL'
+    if world == False:
+        constraint.target_space = 'LOCAL'
+        constraint.owner_space = 'LOCAL'
+    else:
+        constraint.target_space = 'WORLD'
+        constraint.owner_space = 'WORLD'
     constraint.influence = influence
-
+    if head_tail == 1:
+        constraint.head_tail = 1
 def select_bone(bone):
     bone.select = True
     #bone.select_head = True
@@ -81,6 +100,7 @@ class STORM_Adapt_Operator(bpy.types.Operator):
         new_armature = new_object.data.id_data.copy()
         new_armature.name = new_object.name
         new_object.data = new_armature
+        new_armature.display_type = 'OCTAHEDRAL'
         context.view_layer.objects.active = new_object
         bpy.ops.object.mode_set(mode='EDIT', toggle=False)
         with open("ParentDict.json", "r") as dictionary:
@@ -196,22 +216,73 @@ class STORM_Rig_Generator(bpy.types.Operator):
     def execute(self, context):
         # 0.276
         edit_bones = context.active_object.data.edit_bones
+        # TORSO
         copy_bone_props(new_bone_name="TORSO", old_bone=edit_bones["DEF_pelvis"], set_as_parent = False, parent = "DEF_trall", world_bone = True)
         copy_bone_props(new_bone_name="CHEST", old_bone=edit_bones["TORSO"], set_as_parent = False, parent = "TORSO")
         copy_bone_props(new_bone_name="HIPS", old_bone=edit_bones["TORSO"], set_as_parent = False, parent = "TORSO")
         copy_bone_props(new_bone_name="pelvis_TWEAK", old_bone=edit_bones["DEF_pelvis"], length = 0.276, set_as_parent = True, parent = "HIPS")
         copy_bone_props(new_bone_name="spine_TWEAK", old_bone=edit_bones["DEF_spine"], length = 0.276, set_as_parent = True, parent = "CHEST", bone_for_length = "DEF_pelvis")
         copy_bone_props(new_bone_name="spine_FK", old_bone=edit_bones["DEF_spine"], set_as_parent = True, parent = "spine_TWEAK")
-        add_stretch(bone=edit_bones["DEF_pelvis"], bone_to_stetch_to="spine_TWEAK")
+        add_stretch(bone="DEF_pelvis", bone_to_stetch_to="spine_TWEAK")
         bpy.ops.object.mode_set(mode='EDIT', toggle=False)
         copy_bone_props(new_bone_name="MCH_chest", old_bone=edit_bones["DEF_spine"], set_as_parent = False, parent = "spine_FK", world_bone = True)
         copy_bone_props(new_bone_name="spine1_TWEAK", old_bone=edit_bones["DEF_spine1"], length = 0.276, set_as_parent = True, parent = "MCH_chest", bone_for_length = "DEF_pelvis")
         copy_bone_props(new_bone_name="spine1_FK", old_bone=edit_bones["DEF_spine1"], set_as_parent = True, parent = "spine1_TWEAK")
         edit_bones["MCH_chest"].parent = edit_bones["spine_FK"]
-        add_copy_transforms(bone=edit_bones["MCH_chest"], bone_to_stetch_to="CHEST", influence=0.75)
+        add_copy_transforms(bone="MCH_chest", bone_to_stetch_to="CHEST", influence=0.75, constraint_type='COPY_TRANSFORMS')
         bpy.ops.object.mode_set(mode='EDIT', toggle=False)
-        add_stretch(bone=edit_bones["DEF_spine"], bone_to_stetch_to="spine1_TWEAK")
+        add_stretch(bone="DEF_spine", bone_to_stetch_to="spine1_TWEAK")
         bpy.ops.object.mode_set(mode='EDIT', toggle=False)
+        copy_bone_props(new_bone_name="neck_TWEAK", old_bone=edit_bones["DEF_neck"], length = 0.276, set_as_parent = True, parent = "MCH_chest", bone_for_length = "DEF_pelvis")
+        copy_bone_props(new_bone_name="MCH_neck_TWIST", old_bone=edit_bones["neck_TWEAK"], set_as_parent = True, parent = "spine1_FK")
+        copy_bone_props(new_bone_name="MCH_INT_neck", old_bone=edit_bones["DEF_neck"], set_as_parent = False, parent = "DEF_trall")
+        copy_bone_props(new_bone_name="neck", old_bone=edit_bones["neck_TWEAK"], set_as_parent = True, parent = "MCH_INT_neck", bone_for_length = "DEF_neck", length = 1)
+        copy_bone_props(new_bone_name="MCH_neck", old_bone=edit_bones["neck"], set_as_parent = True, parent = "spine1_FK")
+        add_copy_transforms(bone="MCH_neck_TWIST", bone_to_stetch_to="neck", influence=1, constraint_type='COPY_LOCATION', world = True)
+        add_copy_transforms(bone="MCH_neck_TWIST", bone_to_stetch_to="neck", influence=1, constraint_type='DAMPED_TRACK', world = True, head_tail = 1)
+        add_copy_transforms(bone="MCH_neck_TWIST", bone_to_stetch_to="neck", influence=1, constraint_type='COPY_SCALE', world = True)
+        
+        add_copy_transforms(bone="MCH_INT_neck", bone_to_stetch_to="MCH_neck", influence=1, constraint_type='COPY_LOCATION', world = True)
+        add_copy_transforms(bone="MCH_INT_neck", bone_to_stetch_to="MCH_neck", influence=1, constraint_type='COPY_SCALE', world = True)
+        add_copy_transforms(bone="MCH_INT_neck", bone_to_stetch_to="MCH_neck", influence=1, constraint_type='COPY_ROTATION', world = True)
+        bpy.ops.object.mode_set(mode='EDIT', toggle=False)
+        copy_bone_props(new_bone_name="head_TWEAK", old_bone=edit_bones["DEF_head"], length = 0.276, set_as_parent = True, bone_for_length = "DEF_pelvis")
+        copy_bone_props(new_bone_name="MCH_INT_head", old_bone=edit_bones["DEF_head"], set_as_parent = False, parent = "DEF_trall")
+        copy_bone_props(new_bone_name="head", old_bone=edit_bones["head_TWEAK"], set_as_parent = True, parent = "MCH_INT_head", bone_for_length = "DEF_head", length = 1)
+        copy_bone_props(new_bone_name="MCH_head", old_bone=edit_bones["head"], set_as_parent = True, parent = "neck")
+        copy_bone_props(new_bone_name="head_TIP_TWEAK", old_bone=edit_bones["DEF_head"], length = 0.276, set_as_parent = False, bone_for_length = "DEF_pelvis", parent = "head_TWEAK", world_bone = True, align=True)
+        add_copy_transforms(bone="MCH_INT_head", bone_to_stetch_to="MCH_head", influence=1, constraint_type='COPY_LOCATION', world = True)
+        add_copy_transforms(bone="MCH_INT_head", bone_to_stetch_to="MCH_head", influence=1, constraint_type='COPY_SCALE', world = True)
+        add_copy_transforms(bone="MCH_INT_head", bone_to_stetch_to="MCH_head", influence=1, constraint_type='COPY_ROTATION', world = True)
+        add_stretch(bone="DEF_spine1", bone_to_stetch_to="neck_TWEAK")
+        add_copy_transforms(bone="DEF_neck", bone_to_stetch_to="head", influence=0.75, constraint_type='COPY_ROTATION', world = True)
+        add_stretch(bone="DEF_neck", bone_to_stetch_to="head_TWEAK")
+        add_stretch(bone="DEF_head", bone_to_stetch_to="head_TIP_TWEAK")
+        
+        # ARMS
+        bpy.ops.object.mode_set(mode='EDIT', toggle=False)
+        
+        copy_bone_props(new_bone_name="MCH_ARM_SOCKET.L", old_bone=edit_bones["DEF_clavicle.L"], set_as_parent = False, parent = "DEF_clavicle.L", world_bone = True)
+        copy_bone_props(new_bone_name="MCH_INT_ARM_SOCKET.L", old_bone=edit_bones["DEF_clavicle.L"], set_as_parent = False, parent = "DEF_trall", world_bone = True)
+        
+        copy_bone_props(new_bone_name="MCH_SWITCH_upperarm.L", old_bone=edit_bones["DEF_upperarm.L"], set_as_parent = False, parent="MCH_INT_ARM_SOCKET.L")
+        copy_bone_props(new_bone_name="upperarm_TWEAK.L", old_bone=edit_bones["DEF_upperarm.L"], length = 0.276, set_as_parent = True, bone_for_length = "DEF_pelvis")
+        copy_bone_props(new_bone_name="MCH_upperarm_TWEAK.L", old_bone=edit_bones["upperarm_TWEAK.L"], set_as_parent = True, parent = "MCH_SWITCH_upperarm.L")
+        copy_bone_props(new_bone_name="upperarm_FK.L", old_bone=edit_bones["DEF_upperarm.L"], set_as_parent = False, parent="MCH_INT_ARM_SOCKET.L")
+        
+        copy_bone_props(new_bone_name="MCH_SWITCH_forearm.L", old_bone=edit_bones["DEF_forearm.L"], set_as_parent = False, parent="MCH_SWITCH_upperarm.L")
+        copy_bone_props(new_bone_name="forearm_TWEAK.L", old_bone=edit_bones["DEF_forearm.L"], length = 0.276, set_as_parent = True, bone_for_length = "DEF_pelvis")
+        copy_bone_props(new_bone_name="MCH_forearm_TWEAK.L", old_bone=edit_bones["forearm_TWEAK.L"], set_as_parent = True, parent = "MCH_SWITCH_forearm.L")
+        copy_bone_props(new_bone_name="forearm_FK.L", old_bone=edit_bones["DEF_forearm.L"], set_as_parent = False, parent="upperarm_FK.L")
+        
+        copy_bone_props(new_bone_name="MCH_SWITCH_hand.L", old_bone=edit_bones["DEF_forearm.L"], set_as_parent = False, parent="MCH_SWITCH_forearm.L")
+        copy_bone_props(new_bone_name="hand_TWEAK.L", old_bone=edit_bones["DEF_hand.L"], length = 0.276, set_as_parent = True, bone_for_length = "DEF_pelvis")
+        copy_bone_props(new_bone_name="MCH_hand_TWEAK.L", old_bone=edit_bones["hand_TWEAK.L"], set_as_parent = True, parent = "MCH_SWITCH_hand.L")
+        copy_bone_props(new_bone_name="hand_FK.L", old_bone=edit_bones["DEF_hand.L"], set_as_parent = False, parent="forearm_FK.L")
+        copy_bone_props(new_bone_name="hand_TIP_TWEAK.L", old_bone=edit_bones["DEF_hand.L"], length = 0.276, set_as_parent = False, bone_for_length = "DEF_pelvis", parent = "hand_TWEAK.L", world_bone = True, align=True)
+        
+
+        
         return {"FINISHED"}
 
 classes = [STORM_Adapt_Operator, STORM_Rig_Generator]
